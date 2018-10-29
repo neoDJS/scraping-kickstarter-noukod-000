356f48e63b0af2e015a7ea2e6da647726b5dff01
Dimanche Jhon Steeve <jsteeve@noukod.com>
2018-10-29 16:45:16 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -4,4 +4,11 @@ require 'Nokogiri'
 def create_project_hash
   # write your code here
   ash ={}
+  # This just opens a file and reads it into a variable
+  html = File.read('fixtures/kickstarter.html')
+
+  kickstarter = Nokogiri::HTML(html)
+  projects = kickstarter.css("li.project.grid_4")
+  project.css("h2.bbcard_name strong a").text
+  ash
 end
